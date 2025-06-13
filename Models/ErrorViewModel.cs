8225c780ca518115cc4befc8f2c92f25aa680e29
jhajhan <jhannasllovit@iskolarngbayan.pup.edu.ph>
2025-06-13 19:40:04 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DIYFilipinoDessert.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
