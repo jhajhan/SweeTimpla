0b4dfc3244f8037dd9a523ab723c0eea7bd645db
pikachuue <manginsaynikki08@gmail.com>
2025-07-01 00:02:06 +0800

    Add front-end draft

---
+++ Views/Home/BuildYourOwn.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DIYFilipinoDessert.Views.Home
+{
+    public class BuildYourOwnModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
